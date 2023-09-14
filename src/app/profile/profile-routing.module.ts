566a6c536893380945dfc80e58c6a233845d2efb
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-09-14 11:03:06 +0100

    route

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
+import { TasksComponent } from './tasks/tasks.component';
 
-const routes: Routes = [{ path: '', component: ProfileComponent }];
+const routes: Routes = [{ path: '', component: ProfileComponent,
+children: [
+  {
+    path:"",component: UserprofileComponent
+  },
+  {path:"task",component:TasksComponent}
+]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
